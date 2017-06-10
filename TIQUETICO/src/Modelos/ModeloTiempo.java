3312ca9a8e4660e80d111f808c0e759e572e3d25
jjomena <joaquinmena.84@gmail.com>
2017-06-10 11:05:33 -0600

    Modulo Tiempo
    Calculo del Tiempo

--- TIQUETICO/src/Modelos/ModeloTiempo.java
+++ TIQUETICO/src/Modelos/ModeloTiempo.java
@@ -7,7 +7,7 @@ import java.util.Calendar;
  * @author Joaquín
  */
 public class ModeloTiempo {
-    Calendar calendario = Calendar.getInstance();
+    
     int tiempoAcumulado=0;
     int hora, minutos, segundos;
     int hora2,minutos2,segundos2;
@@ -15,39 +15,19 @@ public class ModeloTiempo {
     
     
     public void iniciarProceso(){
-        hora = calendario.get(Calendar.HOUR_OF_DAY);
-        minutos = calendario.get(Calendar.MINUTE);
-        segundos = calendario.get(Calendar.SECOND);
-        //
-        /*
-        int hora = 3;
-        int min = 15;
-        int seg = 10;
-        //
-        int hora2 = 3;
-        int min2 = 20;
-        int seg2 = 5;
-        //
-        int parHora = (12-hora)*3600;
-        int parHora2 = (12-hora2)*3600;
-        //
-        int parMin = (60-min)*60;
-        int parMin2 = (60-min2)*60;
-        //
-        int parSeg = (60-seg);
-        int parSeg2 = (60-seg2);
-        //
-        int Calculo = parHora+parMin+parSeg;
-        int Calculo2 = parHora2+parMin2+parSeg2;
-        //
-        tiempoAcumulado = tiempoAcumulado+(Calculo-Calculo2);
-*/
+        Calendar calendario = Calendar.getInstance();
+        this.hora = calendario.get(Calendar.HOUR_OF_DAY);
+        this.minutos = calendario.get(Calendar.MINUTE);
+        this.segundos = calendario.get(Calendar.SECOND);
+        System.out.println("hora "+hora+" minutos "+minutos+" segundos "+segundos);
     }
     
     public void pausarProceso(){
-        hora2 = calendario.get(Calendar.HOUR_OF_DAY);
-        minutos2 = calendario.get(Calendar.MINUTE);
-        segundos2 = calendario.get(Calendar.SECOND);
+        Calendar calendario2 = Calendar.getInstance();
+        this.hora2 = calendario2.get(Calendar.HOUR_OF_DAY);
+        this.minutos2 = calendario2.get(Calendar.MINUTE);
+        this.segundos2 = calendario2.get(Calendar.SECOND);
+        System.out.println("hora "+hora2+" minutos "+minutos2+" segundos "+segundos2);
         //
         int parHora = (12-hora)*3600;
         int parHora2 = (12-hora2)*3600;
@@ -62,6 +42,7 @@ public class ModeloTiempo {
         int Calculo2 = parHora2+parMin2+parSeg2;
         //
         tiempoAcumulado = tiempoAcumulado+(Calculo-Calculo2);
+        System.out.println("TIEMPO ACUMULADO "+tiempoAcumulado);
     }
     
     public String calcularProceso(){
